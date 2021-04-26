bc46eb5d73f155ce25e04db51e8b7b88f5a75a2f
Jamal <jamensky1991@gmail.com>
2021-04-26 21:28:27 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
     validates :content, length:{minimum: 250}
     validates :summary, length:{maximum: 250}
     validates :category, inclusion:{in: %w(Fiction Non-fiction), in: [true, false] }
+
+    validate :title_does_not_contain
+
+     def title_does_not_contain
+        if !title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "not clickbait enough")
+        end
+     end 
 end
