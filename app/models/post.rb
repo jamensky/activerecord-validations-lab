f6054248b3f5c104043e60ae7c3190abf4b22145
Jamal <jamensky1991@gmail.com>
2021-04-27 19:31:59 +0100

    commit tree 3761758eb416770baa88deaab2e8295ffb4da0c7

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,12 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length:{minimum: 250}
     validates :summary, length:{maximum: 250}
-    validates :category, inclusion:{in: %w(Fiction Non-fiction), in: [true, false] }
+    validates :category, inclusion:{in: %w(Fiction Non-Fiction) }
 
     validate :title_is_not_click_baity
 
      def title_is_not_click_baity
-        if title && !title.match(/Won't Believe | Secret | Top [number] | Guess/)
+        if title && !title.match?(/Won't Believe | Secret | Top [number] | Guess/)
             errors.add(:title, "not clickbait enough")
         end
      end 
